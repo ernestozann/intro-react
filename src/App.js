a9eeef83d95eb8e17b9227bcf1a56d5863aecfd6
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-09-21 12:20:26 -0600

    CSS en React

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React from 'react';
-import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch";
-import { TodoList } from "./TodoList";
-import { TodoItem } from "./TodoItem";
-import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 // import './App.css';
 
 const todos = [
-  { text: "Cortar cebolla", completed: false},
-  { text: "Tomar curso intro a React", completed: false},
-  { text: "Llorar con la llorona", completed: false},
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el cursso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'LALALALAA', completed: false },
 ];
 
-function App(props) {
+function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-
-      <TodoSearch/>
+      <TodoSearch />
 
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} />
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
 
-      <CreateTodoButton/>
+      <CreateTodoButton />
     </React.Fragment>
   );
 }
