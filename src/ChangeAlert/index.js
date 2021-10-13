913c55589f722e12439beb5e18def10af83eecf5
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-10-13 12:03:21 -0600

    resolviendo retos de StorageEventListener

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import './ChangeAlert.css';
 
 function ChangeAlert({ show, toggleShow }) {
 	if (show) {
 		return (
-			<div>
-				<p>Hubo cambios!! Cambios por todos lados!!</p>
-				<button
-					onClick={() => toggleShow(false)}
-				>
-					Refresh ToDos
-				</button>
+			<div className="ChangeAlert-bg">
+				<div className="ChangeAlert-container">
+					<p>looks like you change your ToDos in another window or tab</p>
+					<p>Do you want to sincronize your ToDos?</p>
+					<button
+						className="TodoForm-button TodoForm-button--add"
+						onClick={toggleShow}
+					>
+						Yes!
+					</button>
+				</div>
 			</div>
 		);
 	} else {
